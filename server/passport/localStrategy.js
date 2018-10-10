7ef639c74b64cd5bfb9d8cf398d6638566662a15
agdwm <almudenaguerraswm@gmail.com>
2018-10-10 19:55:07 +0200

    Login working

--- server/passport/localStrategy.js
+++ server/passport/localStrategy.js
@@ -1,27 +1,33 @@
-const passport      = require('passport');
+const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-const User          = require('../models/User');
-const bcrypt        = require('bcrypt');
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 passport.use(new LocalStrategy({
-    usernameField: 'username',
-    passwordField: 'password'
-  }, 
-  (username, password, done) => {
-    User.findOne({ username })
-    .then(foundUser => {
-      if (!foundUser) {
-        done(null, false, { message: 'Incorrect username' });
-        return;
-      }
+		usernameField: 'email',
+		passwordField: 'password'
+	},
+	(email, password, done) => {
+		email = email.toLowerCase().trim();
 
-      if (!bcrypt.compareSync(password, foundUser.password)) {
-        done(null, false, { message: 'Incorrect password' });
-        return;
-      }
+		User.findOne({email})
+			.then(foundUser => {
+				if (!foundUser) {
+					done(null, false, {
+						message: 'Incorrect email or password'
+					});
+					return;
+				}
 
-      done(null, foundUser);
-    })
-    .catch(err => done(err));
-  }
+				if (!bcrypt.compareSync(password, foundUser.password)) {
+					done(null, false, {
+						message: 'Incorrect email or password'
+					});
+					return;
+				}
+
+				done(null, foundUser);
+			})
+			.catch(err => done(err));
+	}
 ));
