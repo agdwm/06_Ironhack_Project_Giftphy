e2323f6a9c1558f77cfca40f475d55c610dfe663
agdwm <almudenaguerraswm@gmail.com>
2018-10-14 12:01:37 +0200

    Init validations into Group (name, owner and users)

--- server/models/Group.js
+++ server/models/Group.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 
 const groupSchema = new Schema({
-	groupName: { type: String, required: true, index: true, lowercase: true, trim: true },
+	groupName: { type: String, required: true, index: true, lowercase: true, trim: true, maxlength: 50 },
 	owner: { type: Schema.Types.ObjectId, ref: 'User', required: true },
 	users: [{ type: Schema.Types.ObjectId, ref: 'User' }]
 }, {
--- server/routes/groups/groups.js
+++ server/routes/groups/groups.js
@@ -6,19 +6,121 @@ const Group = require('../../models/Group');
 // (C)RUD -> Create a NEW Group
 router.post('/new', ensureLoggedIn(), (req, res, next) => {
 	//name, owner, (users)??
-	const {name} = req.body;
+	const {groupName} = req.body;
+	let usersSelected = req.body.users;
+	console.log('GROUPNAME', groupName)
 	let owner = req.user;
+	const name_limitChar = 50;
 
-	const newGroup = new Group({ 
-		name, 
-		owner
-	});
+	const isOwnerValid = (gOwner) => {
+		if (gOwner && Object.prototype.toString.call(gOwner) === '[object Object]') {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
-	newGroup.save()
-		.then((group) => {
-			res.status(200).json({group})
-		})
-		.catch(e => next(e));
+	const isGroupNameValid = (gName) => {
+		if (gName && gName !== '') {
+			gName = gName.trim().replace(/[ ]{2,}/gi,' ');
+			if (gName.split('').length <= name_limitChar) {
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			return false;
+		}
+	}
+
+	const areUsersValid = (gUsers) => {
+		if (gUsers && gUsers !== '') {
+			console.log(gUsers)
+			try {
+				JSON.parse(gUsers)
+			} catch(err) {
+				res.status(403).json({message: 'Group must be an Array of objects'}) 
+			}
+			return true
+
+			// console.log('USER', Object.prototype.toString.call(gUsers));
+			// if (Object.prototype.toString.call(gUsers) === '[object Array]') {
+			// 	invalidUser = users.find((gUser) => {
+			// 		console.log('USER', Object.prototype.toString.call(gUser));
+			// 		return Object.prototype.toString.call(gUser) != '[object Object]';
+			// 	})
+
+			// 	if (invalidUser) {
+			// 		console.log('There are some users with an incorrect type');
+			// 		return false;
+			// 	}else {
+			// 		return true;
+			// 	}
+			// } else {
+			// 	return false;
+			// }
+		} else {
+			console.log('There are not users selected');
+			return true;
+		}
+	} 
+
+	const isEachUserValid = (usersParsed) => {
+		if (Object.prototype.toString.call(usersParsed) === '[object Array]') {
+			console.log('YUJU ARRAY');
+			invalidUser = usersParsed.find((gUser) => {
+				console.log('USER', Object.prototype.toString.call(gUser));
+				return Object.prototype.toString.call(gUser) != '[object Object]';
+			})
+		
+			if (invalidUser) {
+				
+				return false;
+			}else {
+				return true;
+			}
+		} else {
+			return false;
+		}
+		
+	}
+
+	const isGroupValid = (owner, groupName, usersSelected) => {
+		if (isOwnerValid(owner)) {
+			if (isGroupNameValid(groupName)) {
+				if (areUsersValid(usersSelected)) {
+					usersSelected = JSON.parse(usersSelected);
+					if (isEachUserValid(usersSelected)) {
+						return true;
+					} else {
+						res.status(403).json({message: 'The is any user with a invalid type'});
+					}
+				} else {
+					res.status(403).json({message: 'The users do not have a valid type'});
+				}
+			} else {
+				res.status(403).json({message: `The group name is required (max ${name_limitChar} characters)`});
+			}
+		} else {
+			res.status(403).json({message: 'You must be logged in to create a new group'});
+		}
+	}
+
+	const groupParams = [owner, groupName, usersSelected];
+
+	if (isGroupValid(...groupParams)) {
+
+	}
+	// const newGroup = new Group({ 
+	// 	groupName,
+	// 	owner
+	// });
+
+	// newGroup.save()
+	// 	.then((group) => {
+	// 		res.status(200).json({group})
+	// 	})
+	// 	.catch(e => next(e));
 });
 
 //C(R)UD -> Retrieve ALL Groups of an User
