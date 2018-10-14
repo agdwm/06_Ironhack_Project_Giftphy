6dcc534159037e93371b965c9e37c105626c1aa2
agdwm <almudenaguerraswm@gmail.com>
2018-10-14 12:00:51 +0200

    Ordered the properties of the model User

--- server/models/User.js
+++ server/models/User.js
@@ -2,30 +2,10 @@ const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 
 const userSchema = new Schema({
-	username: { 
-		type: String,
-		required: true, 
-		index: true, 
-		unique: true, 
-		trim: true
-	},
-	password: { 
-		type: String, 
-		required: true,
-		trim: true
-	},
-	email: { 
-		type: String, 
-		required: true, 
-		index: true, 
-		unique: true, 
-		trim: true, 
-		lowercase: true
-	},
-	profilePic: { 
-		type: String, 
-		default: 'images/default-avatar-500.png' 
-	},
+	username: { type: String, required: true, index: true, unique: true, trim: true	},
+	password: { type: String, required: true, trim: true },
+	email: { type: String, required: true, index: true, unique: true, trim: true, lowercase: true },
+	profilePic: { type: String, default: 'images/default-avatar-500.png' },
 	role: [{ 
 		type: String, 
 		enum: ["owner", "guest"] 
