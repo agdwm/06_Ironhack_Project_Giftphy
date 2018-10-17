cf9167933b5a8aa54f55800ed7783988bb2da5cb
agdwm <almudenaguerraswm@gmail.com>
2018-10-17 11:03:00 +0200

    Added the middleware Cors

--- server/app.js
+++ server/app.js
@@ -12,21 +12,35 @@ const path         = require('path');
 const session    = require("express-session");
 const MongoStore = require('connect-mongo')(session);
 const flash      = require("connect-flash");
-    
+const cors 			= require('cors'); 
 
-mongoose
-  .connect('mongodb://localhost/server', {useNewUrlParser: true})
-  .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-  })
-  .catch(err => {
-    console.error('Error connecting to mongo', err)
-  });
+const { DBURL } = process.env;
+mongoose.connect( DBURL, {useNewUrlParser: true})
+	.then(x => {
+		console.log(`Connected to Mongo on ${DBURL}! Database name: "${x.connections[0].name}"`)
+	})
+	.catch(err => {
+		console.error('Error connecting to mongo', err)
+	});
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
 
 const app = express();
+
+// Middleware Setup
+var whitelist = [
+	'http://localhost:3001',
+	'http://localhost:3000'
+];
+var corsOptions = {
+	origin: function(origin, callback){
+		var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
+		callback(null, originIsWhitelisted);
+	},
+	credentials: true
+};
+app.use(cors(corsOptions));
 
 // Middleware Setup
 app.use(logger('dev'));
@@ -37,9 +51,9 @@ app.use(cookieParser());
 // Express View engine setup
 
 app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
+	src:  path.join(__dirname, 'public'),
+	dest: path.join(__dirname, 'public'),
+	sourceMap: true
 }));
       
 
@@ -50,13 +64,13 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
 hbs.registerHelper('ifUndefined', (value, options) => {
-  if (arguments.length < 2)
-      throw new Error("Handlebars Helper ifUndefined needs 1 parameter");
-  if (typeof value !== undefined ) {
-      return options.inverse(this);
-  } else {
-      return options.fn(this);
-  }
+	if (arguments.length < 2)
+		throw new Error("Handlebars Helper ifUndefined needs 1 parameter");
+	if (typeof value !== undefined ) {
+		return options.inverse(this);
+	} else {
+		return options.fn(this);
+	}
 });
   
 
@@ -66,10 +80,10 @@ app.locals.title = 'Express - Generated with IronGenerator';
 
 // Enable authentication using session + passport
 app.use(session({
-  secret: 'irongenerator',
-  resave: true,
-  saveUninitialized: true,
-  store: new MongoStore( { mongooseConnection: mongoose.connection })
+	secret: 'irongenerator',
+	resave: true,
+	saveUninitialized: true,
+	store: new MongoStore( { mongooseConnection: mongoose.connection })
 }))
 app.use(flash());
 require('./passport')(app);
